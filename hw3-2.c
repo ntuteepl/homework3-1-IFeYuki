eed9e1ce869e23be1d4268eb0e1372da23a1f1ad
IFeYuki <146065143+IFeYuki@users.noreply.github.com>
2023-10-18 16:28:44 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,17 +3,27 @@
 
 int main()
 {
-    int s1,d1,s2,d2,s3,d3,reg,num=1;
+    int s1,d1,s2,d2,s3,d3,num=3;
     scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
-    
-    if (d1>s2 && d1>s3 && d2<=s3)
-        num++;
-    else if (d1>s2 && d2>s3 && d1<=s3)
-        num++;
-    else if (d1>s2 && d2>s3 && d3<=d2)
-        num++;
-    else if (d1>s2 && d2>s3 && d1>s3)
-        num+=2;
+
+    if (d1<=s2 && d2<=s3)      // 1,2,3
+        num-=2;
+    else if (d1<=s3 && d3<=s2) // 1,3,2
+        num-=2;
+    else if (d2<=s1 && d1<=s3) // 2,1,3
+        num-=2;
+    else if (d2<=s3 && d3<=s1) // 2,3,1
+        num-=2;
+    else if (d3<=s1 && d1<=s2) // 3,1,2
+        num-=2;
+    else if (d3<=s2 && d2<=s1) // 3,2,1
+        num-=2;
+    else if (d1<=s2 || d1<=s3)
+        num--;
+    else if (d2<=s3 || d2<=s1)
+        num--;
+    else if (d3<=s1 || d3<=s2)
+        num--;
 
     printf("%d",num);
     return 0;
