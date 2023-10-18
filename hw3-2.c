01d51b656c3ad8424b220aa8e689076465f737c1
IFeYuki <146065143+IFeYuki@users.noreply.github.com>
2023-10-18 16:45:45 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,7 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, charargv[]) {
+int main(int argc, char*argv[]) {
 int num=3,s1,s2,s3,d1,d2,d3;
 scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
 if(s1>=d2||s1>=d3)
