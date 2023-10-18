fdbdaa202bec86b7fa8824c0f72e5bb7dc91582c
IFeYuki <146065143+IFeYuki@users.noreply.github.com>
2023-10-18 16:33:56 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -19,7 +19,7 @@ int main()
     else if (d3<=s2 && d2<=s1) // 3,2,1
         num-=2;
     else if (d1<=s2 || d1<=s3)
-        num--;
+        num-=1;
     else if (d2<=s3 || d2<=s1)
         num--;
     else if (d3<=s1 || d3<=s2)
