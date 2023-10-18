6ec814ee085b45219a69728e6f0f2142fff183bf
IFeYuki <146065143+IFeYuki@users.noreply.github.com>
2023-10-18 12:57:11 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,12 +3,14 @@
 
 int main()
 {
-    int s1,d1,s2,d2,s3,d3,num=1;
+    int s1,d1,s2,d2,s3,d3,reg,num=1;
     scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
-
+    
     if (d1>s2 && d1>s3 && d2<=s3)
         num++;
     else if (d1>s2 && d2>s3 && d1<=s3)
+        num++;
+    else if (d1>s2 && d2>s3 && d3<=d2)
         num++;
     else if (d1>s2 && d2>s3 && d1>s3)
         num+=2;
