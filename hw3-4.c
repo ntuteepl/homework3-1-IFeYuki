222e74aac30de8d7631b927e3e1c257d5911fd9b
IFeYuki <146065143+IFeYuki@users.noreply.github.com>
2023-10-18 12:44:14 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -3,35 +3,48 @@
 
 int main()
 {
-    int a,a0,a1,a2,a3,b,b0,b1,b2,b3,sa,sb;
-    scanf("%d%d",&a,&b);
-
-    a3=a/1000;
-    a2=(a%1000)/100;
-    a1=((a%1000)/100)/10;
-    a0=a%10;
-
-    b3=b/1000;
-    b2=(b%1000)/100;
-    b1=((b%1000)/100)/10;
-    b0=b%10;
-
-    sa=a0+a1+a2+a3;
-    sb=b0+b1+b2+b3;
-
-    if(sa=sb)
+    int a[100],b[4],ans[4],A=0,B=0,i,j,k,m,x=-1;
+    for(i=0;i<8;i++)
     {
-        if(a>b)
-        printf("%d",b);
-        else
-        printf("%d",a);
+        scanf("%d",&a[i]);
+        x+=1;
+        if (a[i]==0)
+        i=8;
     }
 
-    else
+    for(j=0;j<=3;j++)
     {
-        if(sa>sb)
-        printf("%d",b);
-        else
-        printf("%d",a);
+        ans[j]=a[0]%10;
+        a[0]=a[0]/10;
     }
+
+    for(i=1;i<x;i++)
+    {
+        for(j=0;j<=3;j++)
+        {
+            b[j]=a[i]%10;
+            a[i]=a[i]/10;
+        }
+
+        for(k=0;k<4;k++)
+        {
+            if(b[k]==ans[k])
+            {
+                A+=1;
+            }
+
+            else
+            {
+                for(m=0;m<4;m++)
+                {
+                    if(b[k]==ans[m])
+                    B+=1;
+                }
+            }
+        }
+        printf("%d%c%d%c\n",A,'A',B,'B');
+        A=0; B=0;
+    }
+
+    return 0;
 }
