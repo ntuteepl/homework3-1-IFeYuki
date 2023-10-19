a3ebb6bf3ccc50fa323b20fca643abaab9339bab
IFeYuki <146065143+IFeYuki@users.noreply.github.com>
2023-10-19 10:01:24 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main()
+{
+    int s1,d1,s2,d2,s3,d3,num=3;
+    scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
 
-int main(int argc, char*argv[]) {
-int num=3,s1,s2,s3,d1,d2,d3;
-scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
-if(s1>=d2||s1>=d3)
-num--;
-if(s2>=d1||s2>=d3)
-num--;
-if(s3>=d1||s3>=d2)
-num--;
-printf("%d",num);
-return 0;
+    if (d1<=s2 && d2<=s3)      // 1,2,3
+        num-=2;
+    else if (d1<=s3 && d3<=s2) // 1,3,2
+        num-=2;
+    else if (d2<=s1 && d1<=s3) // 2,1,3
+        num-=2;
+    else if (d2<=s3 && d3<=s1) // 2,3,1
+        num-=2;
+    else if (d3<=s1 && d1<=s2) // 3,1,2
+        num-=2;
+    else if (d3<=s2 && d2<=s1) // 3,2,1
+        num-=2;
+    else if (d1<=s2 || d1<=s3)
+        num--;
+    else if (d2<=s3 || d2<=s1)
+        num--;
+    else if (d3<=s1 || d3<=s2)
+        num--;
+
+    printf("%d",num);
+    return 0;
 }
